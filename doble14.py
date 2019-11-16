6c030278c2ca82a43e6c914c670548691e93ac39
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-16 02:58:59 -0500

    se agrego el python file doble14

---
+++ doble14.py
@@ -0,0 +1,26 @@
+import os
+# MOSTRAR VALORES
+largo=0.0
+ancho=0.0
+
+# INPUT
+largo=float(os.sys.argv[1])
+ancho=float(os.sys.argv[2])
+
+# OUTPUT
+print(largo)
+print(ancho)
+
+# PRECESSING
+area=(largo*ancho)
+
+# VERIFICADOR
+rectangulo=(area>=20)
+
+# CONDICION SIMPLE
+# si el area del rectangulo es mayor o igual que 20 es muy grande
+if(rectangulo==True):
+	print("ES MUY GRANDE")
+else:
+    print("ES PEQUEÑO")
+# fin_if
