ad80a3cccb7167b2654c2dfcd14fe7da0743f98a
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-16 02:49:28 -0500

    se agrego el python file doble11

---
+++ doble11.py
@@ -0,0 +1,23 @@
+import os
+# MOSTRAR VALORES
+nombre=""
+idiomas=0
+
+# INPUT
+nombre=os.sys.argv[1]
+idiomas=int( os.sys.argv[2])
+
+# OUTPUT
+print(nombre)
+print(idiomas)
+
+# VERIFICADOR
+cantidad_idiomas=(idiomas>3)
+
+# CONDICINAL SIMPLE
+# si sabe mas de 3 idiiomas es inteligente
+if(cantidad_idiomas==True):
+    print("eres muy inteligente")
+else:
+    print("sabes pocos idiomas")
+# fin_if
