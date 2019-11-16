bee2ff99614e5433fae2c084745539f7150d76ba
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-16 03:06:08 -0500

    se agrego el python file doble17

---
+++ doble17.py
@@ -0,0 +1,35 @@
+import os
+# AREA DEL TRAPECIO
+# MOSTRAR VALORES
+base1= 0.0
+base2=0.0
+haltura=0.0
+
+# INPUT
+base1=float(os.sys.argv[1])
+base2=float(os.sys.argv[2])
+haltura=float(os.sys.argv[3])
+
+# PROCESSING
+area=haltura*(base1+base2)/2
+
+# VERIFICADOR
+trapecio=(area>12)
+
+# OUTPUT
+print("#################################")
+print("        AREA DEL TRAPECIO        ")
+print("#################################")
+print("BASE NENOR=", base1)
+print("BASE MAYOR=", base2)
+print("HALTURA=", haltura)
+print("#################################")
+print("AREA=", area)
+
+# CONDICION SIMPLE
+# si el trapecio es mayor o igual que 15 es demasiado grande
+if(trapecio==True):
+    print("demasiado grande")
+else:
+    print("muy pequeño")
+# fin_if
