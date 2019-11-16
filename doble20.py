3e1861760444205ecf2048f4959b529766e60c7b
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-16 03:18:23 -0500

    se agrego el python file doble20

---
+++ doble20.py
@@ -0,0 +1,27 @@
+import os
+# HALTURA DE UNA PERSONA
+nombre, masa, mc="", 0.0, 0.0
+
+# INPUT
+nombre=os.sys.argv[1]
+masa=float(os.sys.argv[2])
+mc=float(os.sys.argv[3])
+
+# PROCESSING
+haltura=(masa*mc)/2
+
+# OUTPUT
+print(nombre)
+print(masa)
+print(mc)
+
+# VERIFICADOR
+validez=(haltura<=158.5)
+
+# CONDICION SIMPLE
+# si su haltura es menor o igual a 158.5 es chato
+if(validez==True):
+    print("ES CHATO")
+else:
+    print("HALTURA NORMAL")
+# fin_if
