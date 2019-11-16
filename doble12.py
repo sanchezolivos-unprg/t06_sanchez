666da54bdc0ab9974631346d64457b9d1d57d44c
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-16 02:53:29 -0500

    se agrego el python file doble12

---
+++ doble12.py
@@ -0,0 +1,28 @@
+import os
+# TRIANGULO RECTANGULO
+# MOSTRAR VALORES
+cat1=0.0
+cat2=0.0
+
+# INPUT
+cat1=float(os.sys.argv[1])
+cat2=float(os.sys.argv[2])
+
+# PROCESSING
+import math
+hipotenusa= math.sqrt(cat1**2+cat2**2)
+
+# OUTPUT
+print(cat1)
+print(cat2)
+
+# VERIFICADOR
+triangulo=(hipotenusa>6.0)
+
+# CONDICIONAL SIMPLE
+# si el la hipotenusa es mayor que 10 es muy grande
+if(triangulo==True):
+    print("ES GRANDE")
+else:
+    print("ES PEQUEÑO")
+# fin_if
