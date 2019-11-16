cbfbc696da746f68ae7388d8901609a5bc62309f
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-16 13:21:54 -0500

    se agrego el python file simple14

---
+++ multiple14.py
@@ -0,0 +1,25 @@
+import os
+# validor AL INGRESO A UNA CUENTA WEB
+# MOSTRAR VALORES
+usuario=""
+contraseña=""
+
+# INPUT
+usuario=os.sys.argv[1]
+contraseña=os.sys.argv[2]
+
+# OUTPUT
+print(usuario)
+print(contraseña)
+
+# CONDICION MULTIPLE
+# si las siguientes condicion multiple son validadas se cumple que
+if(usuario=="damian" and contraseña=="$%srt"):
+    print("cuenta OK")
+
+if(usuario=="damian" and contraseña=="tortuga"):
+    print("contraseña denegada")
+
+if(usuario=="guzman" and contraseña=="$%srt"):
+    print("usuario denegado")
+# fin_if
