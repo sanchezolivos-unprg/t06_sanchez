aad5c6c1745d47f54bec07284f680f0db416bb84
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-16 20:23:34 -0500

    se agrego el file lote.bat de 3 validores por cada condicional

---
+++ lote.bat
@@ -0,0 +1,299 @@
+python simple01.py 100.5 20.2 98.6 80.5 100.0
+python simple01.py 10.5 20.2 8.6 60.7 57.8
+python simple01.py 111.4 29.9 9.6 75.5 15.8
+
+python simple02.py "rodrygo" "manzanas" 4.5 20
+python simple02.py "rodrygo" "fresas" 5.5 40
+python simple02.py "rodrygo" "sandias" 8.4 50
+
+python simple03.py "jose" "45698212" 1.43 1.58 30
+python simple03.py "marcos" "14856598" 1.05 1.48 10
+python simple03.py "junior" "78952135" 1.38 1.59 28
+
+python simple04.py 4.5 8.8 45
+python simple04.py 7.3 9.0 10
+python simple04.py 5.0 4.7 8
+
+python simple05.py "gabriela" 4.5 1.9 25.9 8.5 32.7
+python simple05.py "katy" 3.0 1.5 5.9 2.9 18.5
+python simple05.py "alfonso" 7.8 1.7 23.5 9.4 36.6
+
+python simple06.py 4.9 12.6
+python simple06.py 3.6 1.8
+python simple06.py 2.5 54.7
+
+python simple07.py "messi" 80 30 25 58
+python simple07.py "ronaldo" 50 15 9 30
+python simple07.py "zlatan" 76 36 36 57
+
+python simple08.py "bruno" 10 8 10 12
+python simple08.py "carlos" 6 12 9 5
+python simple08.py "hugo" 8 7 5 10
+
+python simple09.py 3 4
+python simple09.py 17 10
+python simple09.py 10 15
+
+python simple10.py 15.0 4.5
+python simple10.py 12.0 3.5
+python simple10.py 15.0 1.0
+
+python simple11.py 10 10 10 15 10 10 5 8
+python simple11.py 15 20 20 25 36 12 15 25
+python simple11.py 8 10 16 36 36 25 15 18
+
+python simple12.py 4 3.5 15 4.5 3 1.5
+python simple12.py 2 3.5 5 4.5 2 1.5
+python simple12.py 8 3.5 10 4.5 7 1.5
+
+python simple13.py 4.5 4.8
+python simple13.py 4.5 4.5
+python simple13.py 15.9 10.7
+
+python simple14.py "natan" 78.0 5.0
+python simple14.py "torres" 62.5 4.1
+python simple14.py "maria" 52.0 2.5
+
+python simple15.py "gregorio" "sr#&op"
+python simple15.py "karl" "itey%/"
+python simple15.py "leonor" "ftre%"
+
+python simple16.py "jhon" 4
+python simple16.py "diego" 5
+python simple16.py "johana" 2
+
+python simple17.py 200.5 150.4 2
+python simple17.py 85.5 45.4 4
+python simple17.py 98.8 15.8 10
+
+python simple18.py "sandra" 12 10 5  5
+python simple18.py "javy" 3 3 2  4
+python simple18.py "sandra" 11 9 7  8
+
+python simple19.py "juana" 20 12 15 23 19 25 15 17
+python simple19.py "patricio" 2 22 10 8 5 15 16 7
+python simple19.py "miguel" 5 1 2 3 12 25 5 16
+
+python simple20.py "lambayeque" 45.5 25.6 36.6 12.5 10.0
+python simple20.py "reque" 15.4 20.0 10.2 15.0 15.0
+python simple20.py "las delicias" 50.5 36.6 10.6 12.8 8.80
+
+python doble01.py "elton" 12.5 11.5 11.0 15.5
+python doble01.py "thiago" 10.5 10.0 5.5 12.5
+python doble01.py "maria" 8.5 10.5 5.0 13.5
+
+python doble02.py "roberto" "75256984" 24 4.5 100.0 36.6 1.58 5.8
+python doble02.py "rodrigo" "16151248" 36 2.8 16.0 36.6 1.58 5.8
+python doble02.py "raul" "45659825" 58 8.5 25.0 16.7 1.58 5.8
+
+python doble03.py 5.5
+python doble03.py 2.5
+python doble03.py 1.4
+
+python doble04.py 7.8 12.5
+python doble04.py 2.8 5.8
+python doble04.py 5.7 6.4
+
+python doble05.py 4.5 2.0
+python doble05.py 10.5 3.6
+python doble05.py 8.5 8.0
+
+python doble06.py "MI CASA" "bances" 20.4 3.5 36.8 30.0
+python doble06.py "tortuga" "julio" 40.4 6.5 60.8 66.0
+python doble06.py "el camaron" "xoana" 24.4 7.5 45.9 36.5
+
+python doble07.py "eugenio" 4 5
+python doble07.py "juan" 8 8
+python doble07.py "marta" 10 10
+
+python doble08.py 10.5 4.5
+python doble08.py 14.5 5.8
+python doble08.py 15.5 6.5
+
+python doble09.py 120.0 100.0 3
+python doble09.py 11.0 52.0 6
+python doble09.py 15.0 12.0 8
+
+python doble10.py "ELISEO" 8 12 8 8
+python doble10.py "DIANA" 10 15 10 10
+python doble10.py "GORGE" 2 1 1 3
+
+python doble11.py "humberto" 3
+python doble11.py "xiomara" 6
+python doble11.py "vilma" 4
+
+python doble12.py 3 4
+python doble12.py 7 14
+python doble12.py 12 12
+
+python doble13.py "aguero" 50 36 12 20
+python doble13.py "pele" 75 50 10 45
+python doble13.py "hazard" 30 25 5 12
+
+python doble14.py 4.0 5.0
+python doble14.py 7.0 8.0
+python doble14.py 6.6 8.82
+
+python doble15.py "frank" "75256584" 1.48 1.56 15.5
+python doble15.py "hugo" "123454678" 1.45 1.51 50.5
+python doble15.py "jack" "45659878" 1.42 1.63 30.02
+
+python doble16.py 25.5 50.2 15.6 84.2 25.8
+python doble16.py 50.5 50.2 55.6 80.0 60.8
+python doble16.py 36.5 30.5 78.0 64.0 45.0
+
+python doble17.py 4.5 8.5 2.5
+python doble17.py 1.5 7.5 1.5
+python doble17.py 2.5 6.0 1.5
+
+python doble18.py "diana" 2.8 6.2 15.5 1.56 4.8
+python doble18.py "ana" 1.5 4.0 12.0 1.52 1.5
+python doble18.py "veronica" 0.8 2.0 8.0 1.45 3.5
+
+python doble19.py 15 25 20 30 36 25 12 10
+python doble19.py 5 10 8 9 6 4 10 15
+python doble19.py 20 22 30 45 25 20 15 14
+
+python doble20.py "WILFREDO" 78.0 4.5
+python doble20.py "WILFREDO" 58.7 4.5
+python doble20.py "WILFREDO" 82.0 4.3
+
+python multiple01.py 10.5 12.5 10.2 13.5 15.0
+python multiple01.py 50.0 50.5 40.0 30.4 50.0
+python multiple01.py 100.5 120.5 100.1 111.0 90.0
+
+python multiple02.py "viviana" "mangos" 4.2 20
+python multiple02.py "vanesa" "maracuya" 5.2 10
+python multiple02.py "lisbeth" "limones" 3.2 80
+
+python multiple03.py "daniel" "23458796" 1.2 0.5 6.2
+python multiple03.py "gabriel" "69584712" 1.8 2.5 10.2
+python multiple03.py "mateo" "19376525" 2.0 2.4 20.0
+
+python multiple04.py 1.2 2.5 5.0
+python multiple04.py 12.0 3.4 2.0
+python multiple04.py 5.0 7.2 6.6
+
+python multiple05.py "diego" 0.5 0.2 1.5 1.3 0.5
+python multiple05.py "tomas" 1.8 4.3 1.7 4.0 1.8
+python multiple05.py "gustavo" 2.5 18.5 2.6 5.3 3.6
+
+python multiple06.py 1.8 6.5
+python multiple06.py 5.8 4.5
+python multiple06.py 8.6 7.0
+
+python multiple07.py "MARADONA" 75 30 25 45
+python multiple07.py "chicharito" 25 15 20 14
+python multiple07.py "guerrero" 30 6 12 11
+
+python multiple08.py "torres" 0 1 2 0
+python multiple08.py "pedro" 2 1 2 2
+python multiple08.py "cachique" 6 3 4 5
+
+python multiple09.py 8 10
+python multiple09.py 12 11
+python multiple09.py 15 20
+
+python multiple10.py 5.2 8.6
+python multiple10.py 17.5 7.8
+python multiple10.py 32.2 6.5
+
+python multiple11.py 10 10 25 10 23 45 0 8
+python multiple11.py 12 10 25 10 13 15 0 5
+python multiple11.py 30 40 20 10 15 35 20 38
+
+python multiple12.py 1 0.5 2 1.5 2 1.8
+python multiple12.py 2 0.8 3 2.5 4 2.5
+python multiple12.py 36 0.9 12 3.5 7 2.8
+
+python multiple13.py 15 15
+python multiple13.py 25 30
+python multiple13.py 75 60
+
+python multiple14.py "damian" "$%srt"
+python multiple14.py "damian" "tortuga"
+python multiple14.py "guzman" "$%srt"
+
+python multiple15.py "franco" 0 0 0 0 0 0 0 0
+python multiple15.py "diego" 2 1 1 3 5 1 1 2
+python multiple15.py "abner" 10 10 10 10 10 10 10 10
+
+python multiple16.py "arnold" 100 100 100 95 101
+python multiple16.py "natacha" 90 80 90 95 100
+python multiple16.py "lisa" 80 75 78 95 75
+
+python multiple17.py "ulises" 80 76
+python multiple17.py "fernando" 98 96
+python multiple17.py "rodriguez" 101 94
+
+python multiple18.py "marcos" 41.8
+python multiple18.py "michael" 38.5
+python multiple18.py "augusto" 36.0
+
+python multiple19.py "admin" "12345"
+python multiple19.py "admin" "45862"
+python multiple19.py "horberg" "12328"
+
+python multiple20.py "jeferson" 150
+python multiple20.py "edinson" 93
+python multiple20.py "anderson" 75
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
